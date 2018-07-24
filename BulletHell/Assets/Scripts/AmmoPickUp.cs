ba9855ac5d33440be7413750a7d6bf3b8a53b73b
CH3kGameDesign <39175079+CH3kGameDesign@users.noreply.github.com>
2018-07-24 22:55:24 +1000

    Ammo AmmoPickUp

---
+++ BulletHell/Assets/Scripts/AmmoPickUp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickUp : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Player") {
+			other.GetComponentInChildren<Shoot> ().ammo += 20;
+			Destroy (this.gameObject);
+		}
+	}
+}
--- BulletHell/Assets/Scripts/Shoot.cs
+++ BulletHell/Assets/Scripts/Shoot.cs
@@ -1,28 +1,41 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Shoot : MonoBehaviour {
 
 	public bool canShoot;
+	public bool reloading;
 	public int fireTimer;
 	public int fireSpeed;
+	public Text ammoCounter;
+	public int ammo;
+	public int ammoLimit;
 
 	public GameObject bullet;
 	public GameObject target;
 
+	void Update () {
+		ammoCounter.text = "Ammo: " + ammo; 
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () {
 		if (Input.GetMouseButton (0) && canShoot == true) {
-			Debug.Log ("BOOOM");
-			canShoot = false;
-			fireTimer = 0;
-			Instantiate (bullet, transform.position, transform.rotation);
+			if (ammo > 0) {
+				Debug.Log ("BOOOM");
+				canShoot = false;
+				fireTimer = 0;
+				ammo -= 1;
+				Instantiate (bullet, transform.position, transform.rotation);
+			}
 		}
 
-		if (fireTimer > fireSpeed)
+		if (fireTimer > fireSpeed && reloading == false)
 			canShoot = true;
 
 		fireTimer++;
 	}
+		
 }
